59922da78adc7639f8e8ef47556658a7617b5988
Vineet Kumar <jammackp@gmail.com>
2022-05-16 00:27:39 +0530

    fix: mockman-js declaration given

--- src/pages/MockmanTest/index.tsx
+++ src/pages/MockmanTest/index.tsx
@@ -2,7 +2,7 @@
 we encourage to have a dedicated route called `/mock-api` on your frontend app where you can import Mockman and test them 
 */
 
-const Mockman = require('mockman-js');
+import Mockman from 'mockman-js';
 export function MockmanTest() {
   return (
     <div className="MockAPI">
