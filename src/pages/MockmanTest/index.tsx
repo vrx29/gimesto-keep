9094aebdb46b18f73d641d1027d17d4ed645a7f1
Vineet Kumar <jammackp@gmail.com>
2022-05-16 00:53:42 +0530

    fix: fix 1 for mockman-js import type error

--- src/pages/MockmanTest/index.tsx
+++ src/pages/MockmanTest/index.tsx
@@ -2,7 +2,6 @@
 we encourage to have a dedicated route called `/mock-api` on your frontend app where you can import Mockman and test them 
 */
 
-// @ts-ignore
 import Mockman from 'mockman-js';
 export function MockmanTest() {
   return (
