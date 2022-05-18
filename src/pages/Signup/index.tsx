347b1c695693ce1479a0f55634fea4f55aba71ae
Vineet Kumar <jammackp@gmail.com>
2022-05-18 23:01:16 +0530

    feat: profile page and logout functionality added

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -111,7 +111,7 @@ export function Signup() {
             <button
               type="submit"
               onClick={handleSignUp}
-              className="text-white bg-teal-600 hover:bg-teal-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+              className="w-full text-white bg-teal-600 hover:bg-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
               Sign Up
             </button>
           </form>
